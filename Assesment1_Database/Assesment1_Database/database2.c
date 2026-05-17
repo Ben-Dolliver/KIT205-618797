de188c9596522b863ec10187833790c20c0a6f5b
BenD0ver2 <42822883+BenD0ver2@users.noreply.github.com>
2026-05-17 19:14:33 +1000

    completed print functions

--- Assesment1_Database/Assesment1_Database/database2.c
+++ Assesment1_Database/Assesment1_Database/database2.c
@@ -113,8 +113,7 @@ void printP2Authors(P2Database* db) {
         // traverse collision chain
         while (current != NULL) {
 
-            printf("Author ID: %d\n",
-                current->authorID);
+            printf("Author ID: %d\n", current->authorID);
 
             current = current->next;
         }
@@ -123,14 +122,56 @@ void printP2Authors(P2Database* db) {
 
 void printP2Papers(P2Database* db) {
 
+    printf("\nPapers\n");
+
+    //  traverse hash table
+    for (int i = 0; i < TABLE_SIZE; i++) {
+
+        DTB2Paper* current = db->papers.table[i];
+
+        //  traverse collision chain
+        while (current != NULL) {
+
+            printf("Paper ID: %d\n", current->paperID);
+
+            current = current->next;
+        }
+    }
 }
 
 void printP2PapersByAuthor(P2Database* db, int authorID) {
+    
+    //  find author in hash table
+    DTB2Author* author = findAuthorHash(&db->authors, authorID);
+
+    //  error checking
+    if (author == NULL) {
+        printf("Author not found\n");
+        return;
+    }
+
+    printf("\nPapers by Author ID %d\n", authorID);
+
+    //  print the author's paper BST
+    printPaperBST(author->papers);
 
 }
 
 void printP2AuthorsByPaper(P2Database* db, int paperID) {
+    
+    //  find paper in hash table
+    DTB2Paper* paper = findPaperHash(&db->papers, paperID);
 
+    //  error checking
+    if (paper == NULL) {
+        printf("Paper not found\n");
+        return;
+    }
+
+    printf("\nAuthors for Paper ID %d\n", paperID);
+
+    //  print the paper's author BST
+    printAuthorBST(paper->authors);
 }
 
 
